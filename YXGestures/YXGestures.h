c13a07a109919e5d5add3609d1086a51e94eb5eb
YourtionGuo <yourtion@gmail.com>
2016-09-27 23:56:45 +0800

    init framework

---
+++ YXGestures/YXGestures.h
@@ -0,0 +1,19 @@
+//
+//  YXGestures.h
+//  YXGestures
+//
+//  Created by YourtionGuo on 27/09/2016.
+//  Copyright © 2016 YourtionGuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YXGestures.
+FOUNDATION_EXPORT double YXGesturesVersionNumber;
+
+//! Project version string for YXGestures.
+FOUNDATION_EXPORT const unsigned char YXGesturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXGestures/PublicHeader.h>
+
+
