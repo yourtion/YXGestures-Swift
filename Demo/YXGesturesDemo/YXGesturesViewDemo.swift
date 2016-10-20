89160a4240f270fcc1586e5d014029969ed11afc
YourtionGuo <yourtion@gmail.com>
2016-10-20 21:37:52 +0800

    update demo

--- Demo/YXGesturesDemo/YXGesturesViewDemo.swift
+++ Demo/YXGesturesDemo/YXGesturesViewDemo.swift
@@ -11,63 +11,25 @@ import YXGestures
 
 class ViewController: UIViewController {
     
-    let gestures = YXGestures()
-
+    let gView = YXGesturesView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        gestures.addSymbol(symbol: "V", code: "28")
-        gestures.addSymbol(symbol: "V", code: "46")
-        gestures.addSymbol(symbol: "Z", code: "141")
-        gestures.addSymbol(symbol: "Z", code: "585")
+        
+        gView.frame = self.view.frame
+        self.view.addSubview(gView)
+        
+        gView.gestures.addSymbol(symbol: "V", code: "28")
+        gView.gestures.addSymbol(symbol: "V", code: "46")
+        gView.gestures.addSymbol(symbol: "Z", code: "141")
+        gView.gestures.addSymbol(symbol: "Z", code: "585")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("event begin!")
-        
-        if touches.count == 1 {
-            if let t = touches.first {
-                gestures.points.removeAll()
-                gestures.points.append(t.location(in: self.view))
-            }
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if touches.count == 1 {
-            if let t = touches.first {
-                gestures.points.append(t.location(in: self.view))
-            }
-        }
-        
-        
-        for touch: AnyObject in touches {
-            let t:UITouch = touch as! UITouch
-            print(t.location(in: self.view))
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("event end!")
-        if touches.count == 1 {
-            if let t = touches.first {
-                gestures.points.append(t.location(in: self.view))
-                print(gestures.points)
-                print(gestures.getResult())
-            }
-        }
-        
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("event canceled!")
-    }
-
-
 }
 
