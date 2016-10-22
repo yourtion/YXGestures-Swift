b5dc3388962c8317049e1f46e0eafc046f0df932
YourtionGuo <yourtion@gmail.com>
2016-10-22 17:06:34 +0800

    update demo

--- Demo/YXGesturesDemo/YXGesturesViewDemo.swift
+++ Demo/YXGesturesDemo/YXGesturesViewDemo.swift
@@ -9,24 +9,43 @@
 import UIKit
 import YXGestures
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,YXGesturesViewDelegate {
     
     let gView = YXGesturesView()
+    let textLable = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        textLable.frame = CGRect(x: 100, y: 100, width: self.view.frame.width-200, height: self.view.frame.height-200)
+        textLable.font = UIFont.boldSystemFont(ofSize: 100)
+        textLable.textAlignment = .center
+        
         gView.frame = self.view.frame
+        gView.delegate = self
+        self.view.addSubview(textLable)
         self.view.addSubview(gView)
         
 //        gView.showPath = false
         gView.lineWidth = 5
         gView.lineColor = UIColor.blue
+        
         gView.gestures.addSymbol(symbol: "V", code: 28)
         gView.gestures.addSymbol(symbol: "V", code: 46)
         gView.gestures.addSymbol(symbol: "Z", code: 141)
         gView.gestures.addSymbol(symbol: "Z", code: 585)
+        
+        gView.gestures.addSymbol(symbol: "S", code: 543235)
+        gView.gestures.addSymbol(symbol: "<", code: 54321)
+        gView.gestures.addSymbol(symbol: "<", code: 421)
+        gView.gestures.addSymbol(symbol: ">", code: 12345)
+        gView.gestures.addSymbol(symbol: ">", code: 145)
+
+    }
+    
+    func gesturesResult(string result: String) {
+        textLable.text = result
     }
     
     override func didReceiveMemoryWarning() {
